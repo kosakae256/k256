9125010efb9400cad61e6b211733221b7f79749b
kosakae256 <kosaka256@gmail.com>
2024-11-26 05:33:48 +0000

    build preparing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div className="">
-      <p>k256.dev</p>
-      <p>first page!</p>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h2 className="font-bold text-4xl text-blue-500">k256.dev</h2>
+      <h2 className="font-bold text-4xl text-blue-500">first page!</h2>
+      <h2 className="font-bold text-xl">Preparing...</h2 >
     </div>
   );
 }
